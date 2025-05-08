8fbe3058d87849c959167cc972544c7286d98975
CODERrazan <10700@holbertonstudents.com>
2025-05-08 12:07:50 +0300

    Create factorial.py and fixing

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n
+
+    return result
+
+if len(sys.argv) > 1:
+    print(factorial(int(sys.argv[1])))
